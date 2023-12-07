089d5e20a1e3892352d17a9e45242d4d52edad63
Aurélien Geron <ageron@users.noreply.github.com>
2023-12-07 15:56:52 +1300

    Done day 5, and make utils functions generic

--- src/day05.rs
+++ src/day05.rs
@@ -1,13 +1,104 @@
+use crate::utils::parse_ints;
 use itertools::Itertools;
+use std::ops::Range;
+
+#[derive(Debug)]
+struct ElfMap {
+    source_range: Range<i64>,
+    offset: i64,
+}
+
+fn get_seed_destination(maps: &[ElfMap], source: i64) -> i64 {
+    // For part 1, each number gets mapped to a single number
+    for map in maps {
+        if map.source_range.contains(&source) {
+            return source + map.offset;
+        }
+    }
+    source
+}
+
+fn get_target_ranges(source_ranges: Vec<Range<i64>>, maps: &[ElfMap]) -> Vec<Range<i64>> {
+    // For part 2, each list of ranges gets mapped to another list of ranges
+    source_ranges
+        .iter()
+        .flat_map(|source_range| {
+            let overlapping: Vec<&ElfMap> = maps
+                .iter()
+                .filter(|&map| {
+                    map.source_range.start < source_range.end
+                        && map.source_range.end > source_range.start
+                })
+                .collect();
+            overlapping
+                .iter()
+                .filter_map(|&map| {
+                    let start = map.source_range.start.max(source_range.start) + map.offset;
+                    let end = map.source_range.end.min(source_range.end) + map.offset;
+                    if end > start {
+                        Some(start..end)
+                    } else {
+                        None
+                    }
+                })
+                .chain(overlapping.windows(2).filter_map(|map_pair| {
+                    // these are the spaces between the ranges, with offset 0
+                    let start = map_pair[0].source_range.end.max(source_range.start);
+                    let end = map_pair[1].source_range.start.min(source_range.end);
+                    if end > start {
+                        Some(start..end)
+                    } else {
+                        None
+                    }
+                }))
+                .collect::<Vec<Range<i64>>>()
+        })
+        .collect()
+}
 
 pub fn run(input: &str) {
-    let lines = input
-        .lines()
-        .map(|line| line.parse::<u32>().unwrap())
-        .collect_vec();
+    let mut parts = input.split("\n\n");
+    let seeds: Vec<i64> = parse_ints(parts.next().unwrap(), false);
+    let all_maps: Vec<Vec<ElfMap>> = parts
+        .map(|m| {
+            m.lines()
+                .skip(1)
+                .map(|line| {
+                    let map_nums: Vec<i64> = parse_ints(line, false);
+                    ElfMap {
+                        source_range: map_nums[1]..(map_nums[1] + map_nums[2]),
+                        offset: map_nums[0] - map_nums[1],
+                    }
+                })
+                .sorted_by(|a, b| a.source_range.start.cmp(&b.source_range.start))
+                .collect()
+        })
+        .collect();
+    let closest_location = seeds
+        .iter()
+        .map(|&seed| {
+            all_maps
+                .iter()
+                .fold(seed, |seed, maps| get_seed_destination(maps, seed))
+        })
+        .min()
+        .unwrap();
+    println!("{}", closest_location);
 
-    let score: u32 = lines.iter().sum();
-    println!("{}", score);
-    let score: u32 = lines.iter().sum();
-    println!("{}", score);
+    let seed_ranges = seeds
+        .iter()
+        .tuples::<(_, _)>()
+        .map(|(&start, &length)| start..(start + length))
+        .collect();
+
+    let closest_location = all_maps
+        .iter()
+        .fold(seed_ranges, |source_ranges, maps| {
+            get_target_ranges(source_ranges, maps)
+        })
+        .iter()
+        .map(|range| range.start)
+        .min()
+        .unwrap();
+    println!("{}", closest_location);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,15 @@
 use itertools::Itertools;
+use num::Num;
+use std::str::FromStr;
 
-pub fn parse_ints(input: &str, signed: bool) -> Vec<i64> {
+pub fn parse_ints<T: Num + FromStr>(input: &str, signed: bool) -> Vec<T> {
     input
         .split(|c: char| !(c.is_ascii_digit() || (signed && c == '-')))
         .filter_map(|s| s.parse().ok())
         .collect()
 }
 
-pub fn parse_int_vecs(input: &str, signed: bool) -> Vec<Vec<i64>> {
+pub fn parse_int_vecs<T: Num + FromStr>(input: &str, signed: bool) -> Vec<Vec<T>> {
     input
         .lines()
         .map(|line| parse_ints(line, signed))
