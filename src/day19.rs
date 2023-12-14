24394c23ccd673e59e015fbb526212e6268b111b
Aurélien Geron <ageron@users.noreply.github.com>
2023-12-14 23:20:25 +1100

    Simplify placeholder code

--- src/day19.rs
+++ src/day19.rs
@@ -1,13 +1,8 @@
-use itertools::Itertools;
-
 pub fn run(input: &str) {
-    let lines = input
+    let data: Vec<_> = input
         .lines()
-        .map(|line| line.parse::<u32>().unwrap())
-        .collect_vec();
+        .map(|line| line.as_bytes())
+        .collect();
 
-    let score: u32 = lines.iter().sum();
-    println!("{}", score);
-    let score: u32 = lines.iter().sum();
-    println!("{}", score);
+    println!("{:?}", data);
 }
