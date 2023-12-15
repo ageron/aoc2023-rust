08b152f7dd8e615a12221f7d229720dae2f5b635
Aurélien Geron <ageron@users.noreply.github.com>
2023-12-15 18:08:53 +1100

    Simplify placeholder code

--- src/day21.rs
+++ src/day21.rs
@@ -1,8 +1,5 @@
 pub fn run(input: &str) {
-    let data: Vec<_> = input
-        .lines()
-        .map(|line| line.as_bytes())
-        .collect();
+    let data: Vec<_> = input.lines().map(|line| line.as_bytes()).collect();
 
     println!("{:?}", data);
 }
